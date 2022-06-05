356cea1cc5aae0997045d1ed0568f39effe7ca7a
Guna Manasa Arjun <guna.manasa.arjun@gmail.com>
2022-06-06 01:58:27 +0530

    Test cases for the entities user and item

--- mappingvalues_1/src/test/java/com/arjun/springhibernatejpa/MappingValuesSpringDataJPATest.java
+++ mappingvalues_1/src/test/java/com/arjun/springhibernatejpa/MappingValuesSpringDataJPATest.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
+import org.hibernate.type.descriptor.java.LocaleTypeDescriptor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.arjun.springhibernatejpa.configuration.SpringDataConfiguration;
+import com.arjun.springhibernatejpa.model.Address;
+import com.arjun.springhibernatejpa.model.AuctionType;
 import com.arjun.springhibernatejpa.model.Item;
 import com.arjun.springhibernatejpa.model.User;
 import com.arjun.springhibernatejpa.repository.ItemRepository;
@@ -39,7 +42,8 @@ public class MappingValuesSpringDataJPATest {
     void storeLoadEntities() {
 
         User user = new User();
-        user.setUsername("username");
+        user.setUsername("abc user");
+        user.setHomeAddress(new Address("abc street", "12345", "abc city"));
         userRepository.save(user);
 
         Item item = new Item();
@@ -53,12 +57,19 @@ public class MappingValuesSpringDataJPATest {
 
         assertAll(
                 () -> assertEquals(1, users.size()),
-                () -> assertEquals("username", users.get(0).getUsername()),
+                () -> assertEquals("abc user", users.get(0).getUsername()),
+                () -> assertEquals("abc street", user.getHomeAddress().getStreet()),
+                () -> assertEquals("12345", user.getHomeAddress().getZipcode()),
+                () -> assertEquals("abc city", user.getHomeAddress().getCity()),
                 () -> assertEquals(1, items.size()),
                 () -> assertEquals("AUCTION: Some Item", items.get(0).getName()),
+                () -> assertEquals(AuctionType.HIGHEST_BID, items.get(0).getAuctionType()),
                 () -> assertEquals("descriptiondescription", items.get(0).getDescription()),
                 () -> assertEquals("descriptiond...", items.get(0).getShortDescription()),
-                () -> assertEquals(2.0, items.get(0).getMetricWeight())
+                () -> assertEquals(2.0, items.get(0).getMetricWeight()),
+                () -> assertEquals(LocalDate.now(), items.get(0).getCreatedOn()),
+                () -> assertTrue(ChronoUnit.SECONDS.between(LocalDateTime.now(),items.get(0).getLastModified()) < 1),
+                () -> assertEquals(new BigDecimal("1.00"), items.get(0).getInitialPrice())
         );
 
     }
