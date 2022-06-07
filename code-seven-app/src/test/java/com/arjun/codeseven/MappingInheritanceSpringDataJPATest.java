a171efb32839c8c614f7896c3dbb73f278ffb877
Guna Manasa Arjun <guna.manasa.arjun@gmail.com>
2022-06-08 02:58:05 +0530

    Added test cases for Mapping for table per concrete class: union

--- code-seven-app/src/test/java/com/arjun/codeseven/MappingInheritanceSpringDataJPATest.java
+++ code-seven-app/src/test/java/com/arjun/codeseven/MappingInheritanceSpringDataJPATest.java
@@ -10,8 +10,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.arjun.codeseven.configuration.SpringDataConfiguration;
 import com.arjun.codeseven.model.BankAccount;
+import com.arjun.codeseven.model.BillingDetails;
 import com.arjun.codeseven.model.CreditCard;
 import com.arjun.codeseven.repositories.BankAccountRepository;
+import com.arjun.codeseven.repositories.BillingDetailsRepository;
 import com.arjun.codeseven.repositories.CreditCardRepository;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,16 +28,21 @@ public class MappingInheritanceSpringDataJPATest {
 	@Autowired
 	private BankAccountRepository bankAccountRepo;
 	
+	@Autowired
+    private BillingDetailsRepository<BillingDetails, Long> billingDetailsRepository;
+	
 	@Test
 	void storeLoadEntities() {
+		
 		
 		CreditCard creditcard = new CreditCard("abc owner", "1234567890123456", "12", "32");
 		creditCardRepo.save(creditcard);
 		
-		BankAccount ba = new BankAccount("abc owner", "account one", "abc bank","abc swift");
+		BankAccount ba = new BankAccount("abc owner1", "account one", "abc bank","abc swift");
 		bankAccountRepo.save(ba);
 		
-		
+		// TABLE PER CONCRETE CLASS WITH INHERENT POLYMORPHMISM: NOT RECOMENDED 
+		/*
 		List<CreditCard> cards = creditCardRepo.findByOwner("abc owner");
 		List<BankAccount> accounts = bankAccountRepo.findByOwner("abc owner");
 		
@@ -51,7 +58,16 @@ public class MappingInheritanceSpringDataJPATest {
 				() -> assertEquals(1, accounts.size()),
 				() -> assertEquals("abc swift", accounts2.get(0).getSwift()),
 				() -> assertEquals(1, accounts2.size()));
+		*/
 		
+		//TABLE PER CONCRETE CLASS WITH UNIONS
+		
+		List<BillingDetails> det = billingDetailsRepository.findByOwner("abc owner1");
+		
+		assertAll(
+				() -> assertEquals(2, billingDetailsRepository.findAll().size()),
+				() -> assertEquals(1, billingDetailsRepository.findByOwner("abc owner").size()),
+				() -> assertEquals("abc user1", det.get(0).getOwner()));
 	}
 	
 	
